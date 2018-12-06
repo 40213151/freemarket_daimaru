dd2752a6d830aab5894ae826ffe05d884370f195
40213151 <daimaruplay@gmail.com>
2018-12-06 17:02:08 +0900

    add_controller_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'items#index'
   get "users/login" => 'users#login'
   get "users/logout" => 'users#logout'
+  get "users/new" => 'users#new'
   get "users/1" => 'users#show'
   get "users/edit" => 'users#edit'
 end
