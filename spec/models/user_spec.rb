0e0eb8c93c7445768d1ef04ae1e822fbb799114f
40213151 <daimaruplay@gmail.com>
2018-12-11 15:14:47 +0900

    edit_user_test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,10 +18,23 @@ require 'rails_helper'
         expect(user.errors[:email]).to include("can't be blank")
       end
 
+      it "is invalid without a password" do
+        user = build(:user, password: nil)
+        user.valid?
+        expect(user.errors[:password]).to include("can't be blank")
+      end
+
       it "is invalid without a password_confirmation although with a password" do
         user = build(:user, password_confirmation: "")
         user.valid?
         expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      end
+
+      it "is invalid with a duplicate email address" do
+        user = create(:user)
+        another_user = build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors[:email]).to include("has already been taken")
       end
 
       it "is invalid with a nickname that has more than 21 characters" do
