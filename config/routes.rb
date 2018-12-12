ea5e89bcb633d06b50277101adc9620891a1bdee
40213151 <daimaruplay@gmail.com>
2018-12-12 11:50:49 +0900

    edit_collection_to_block

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users, only: [:new,:show,:edit] do
-    get :logout, on: :collection
+    collection do
+      get :search
+    end
   end
 end
