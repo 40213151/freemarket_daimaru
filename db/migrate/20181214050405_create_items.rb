894d74858b67db7878a5cecc52fb1842a869650f
40213151 <daimaruplay@gmail.com>
2018-12-17 16:41:41 +0900

    edit_migrate

--- db/migrate/20181214050405_create_items.rb
+++ db/migrate/20181214050405_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
       t.text :description, null: false, length: {maximum: 1000}
       t.string :condition, null: false
       t.string :delivery_method, null: false
-      t.integer :delivery_days, nul: false
+      t.integer :delivery_days, null: false
       t.string :price, null: false
       t.integer :brand_id, foreign_key: true
       t.references :user, foreign_key: true
